57998347c6dfd2edcf2a1168e4aeb25b26ebcb4a
xwlasxd <xwlasxd@xwlasxd.local>
2026-05-31 11:00:57 +1200

    Fix mongodb build error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,11 +1,21 @@
 import { MongoClient } from 'mongodb';
-const uri = process.env.MONGODB_URI as string;
+
+const uri = process.env.MONGODB_URI || '';
+
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
+
 declare global { var _mongoClientPromise: Promise<MongoClient> | undefined; }
-if (!process.env.MONGODB_URI) throw new Error('Please add your MongoDB URI to .env.local');
+
 if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) { client = new MongoClient(uri); global._mongoClientPromise = client.connect(); }
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
   clientPromise = global._mongoClientPromise;
-} else { client = new MongoClient(uri); clientPromise = client.connect(); }
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
 export default clientPromise;
